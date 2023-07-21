05e7aa64940d704b4bdd10c73dc8bf07adae632d
grey bird <hitec16@naver.com>
2023-07-21 11:07:56 +0900

    Added new property and changed name of existing porperty of class House

--- houses/models.py
+++ houses/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class House(models.Model):
     """Model definition for Houses"""
     name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
+    prices_per_night = models.PositiveIntegerField()
     description = models.TextField()
     address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(default=True)
