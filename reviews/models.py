8ceeb0eb05768cc63600f786c66fa996b7a7169d
grey bird <hitec16@naver.com>
2023-08-02 15:17:11 +0900

    Created Review model

---
+++ reviews/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from common.models import CommonModel
+
+# Create your models here.
+
+
+class Review(CommonModel):
+    """Review for room or experiences"""
+    user = models.ForeignKey(
+        'users.User', on_delete=models.SET_NULL, null=True,)
+    room = models.ForeignKey('rooms.Room', null=True,
+                             blank=True, on_delete=models.CASCADE)
+    experience = models.ForeignKey(
+        'experiences.Experience', null=True, blank=True, on_delete=models.CASCADE)
+    payload = models.TextField()
+    rating = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.user} / {self.rating}"
