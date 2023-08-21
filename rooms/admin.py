ab2352d5c72cd97dc6286368a72ec5f6283de793
grey bird <hitec16@naver.com>
2023-08-21 10:29:48 +0900

    ORM methods for Room model

--- rooms/admin.py
+++ rooms/admin.py
@@ -10,9 +10,9 @@ class RoomAdmin(admin.ModelAdmin):
         'name',
         'price',
         'kind',
+        'total_amenities',
         'owner',
         'created_at',
-        'updated_at',
     )
     list_filter = (
         "country",
@@ -24,6 +24,10 @@ class RoomAdmin(admin.ModelAdmin):
         'created_at',
         'updated_at',
     )
+
+    # def total_amenities(self, room):
+    #     print(room)
+    #     return room.amenities.count()
 
 
 @admin.register(Amenity)
