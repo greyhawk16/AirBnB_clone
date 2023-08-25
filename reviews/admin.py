0ea299c1c5006df725aee1283bd13199a7093bae
grey bird <hitec16@naver.com>
2023-08-25 09:11:40 +0900

    Added custom filter for reviews

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,5 +1,26 @@
+from typing import Any, List, Optional, Tuple
 from django.contrib import admin
+from django.db.models.query import QuerySet
 from .models import Review
+
+
+class WordFilter(admin.SimpleListFilter):
+    title = 'Filter By Words'
+    parameter_name = 'BB-61'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('good', 'Good'),
+            ('great', 'Great'),
+            ('awsome', 'Awsome'),
+        ]
+
+    def queryset(self, request, reviews):
+        word = self.value()
+        if word:
+            return reviews.filter(payload__contains=word)
+        else:
+            return reviews
 
 
 # Register your models here.
@@ -10,5 +31,9 @@ class ReviewAdmin(admin.ModelAdmin):
         'payload'
     )
     list_filter = (
+        WordFilter,
         'rating',
+        'user__is_host',
+        'room__category',
+        'room__pet_friendly',
     )
